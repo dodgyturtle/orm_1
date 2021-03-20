969dbb867706d90860c96583fef82c0b15ee5a7b
Aleksandr Semenov <semenlinux@gmail.com>
2021-03-20 21:24:12 +0300

    Print passcards

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ django.setup()
 
 from datacenter.models import Passcard, Visit
 
+def print_passcards_from_db():
+    passcards = Passcard.objects.all()
+    for passcard in passcards:
+        print("owner_name:", passcard.owner_name,"\n",
+        "passcode:", passcard.passcode,"\n",
+        "created_at:", passcard.created_at,"\n",
+        "is_active", passcard.is_active)
+        
 
 if __name__ == "__main__":
     # Программируем здесь
-    print('Все карточки пропусков:', Passcard.objects.all())
+    print_passcards_from_db()
