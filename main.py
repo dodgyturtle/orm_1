7b453205663ae40b683c07fd019545e4084d936a
Aleksandr Semenov <semenlinux@gmail.com>
2021-03-20 21:38:45 +0300

    Filter active passcards

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from datacenter.models import Passcard, Visit
 def print_passcards_from_db():
     passcard_db = Passcard.objects.all()
     print('Количество пропусков:', Passcard.objects.count())
-    passcards = [ passcard.owner_name for passcard in passcard_db if passcard.is_active ]
-    print("Активных пропусков:", len(passcards))
+    active_passcards = Passcard.objects.filter(is_active=True)
+    print("Активных пропусков:", len(active_passcards))
         
 
 if __name__ == "__main__":
