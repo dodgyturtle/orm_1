cc91eafbec88f07e9d3f8dde22ca7c9faba18386
Aleksandr Semenov <semenlinux@gmail.com>
2021-03-22 15:55:35 +0300

    Add README.md and formatting document

--- datacenter/models.py
+++ datacenter/models.py
@@ -11,7 +11,7 @@ class Passcard(models.Model):
     def __str__(self):
         if self.is_active:
             return self.owner_name
-        return f'{self.owner_name} (inactive)'
+        return f"{self.owner_name} (inactive)"
 
 
 class Visit(models.Model):
@@ -24,18 +24,20 @@ class Visit(models.Model):
         return "{user} entered at {entered} {leaved}".format(
             user=self.passcard.owner_name,
             entered=self.entered_at,
-            leaved= "leaved at " + str(self.leaved_at) if self.leaved_at else "not leaved"
+            leaved="leaved at " + str(self.leaved_at)
+            if self.leaved_at
+            else "not leaved",
         )
+
     def get_duration(self):
         user_entered_at = localtime(self.entered_at)
         current_datetime = localtime().replace(microsecond=0)
         time_in_warehouse = current_datetime - user_entered_at
         return time_in_warehouse
-    
+
     def is_visit_long(self, minutes=60):
-        visit_long= self.leaved_at - self.entered_at 
+        visit_long = self.leaved_at - self.entered_at
         visit_long_minutes = visit_long.seconds // 60
         if visit_long_minutes > minutes:
             return True
         return False
-
